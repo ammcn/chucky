ef3235b3847fbfd1c5e8443a932b44b131dc8775
ammonicisne <ammon.15.2015.ci@gmail.com>
2022-09-26 21:48:30 -0600

    Root folder moved

---
+++ Languages/Python/Scripts/first_script.py
@@ -0,0 +1,7 @@
+def WriteToFile():
+    print('### Writing to a file!')
+    # $ py -c 'import first_script; first_script.WriteToTerminal()'
+
+def WriteToTerminal():
+    print('### Writing to a terminal!')
+    # $ py -c 'import first_script; first_script.WriteToTerminal()'
---
+++ Languages/Python/info.md
@@ -0,0 +1,7 @@
+### Python(PY)
+- https://docs.python.org/3/
+
+##### General Info
+- Interpreted language, no interpreter/compilation required, translates directly into machine code
+
+- Can add built in functions, module to interpreter using C for maximum speed(Extensible)
